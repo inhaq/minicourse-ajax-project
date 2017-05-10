365de208b5c501429c089ad834ad4d412460de8b
Inzimam <p126013@nu.edu.pk>
2017-05-11 00:22:43 +0500

    add NYTimes API

--- js/script.js
+++ js/script.js
@@ -17,8 +17,25 @@ function loadData() {
 
     $greeting.text("Hey, you wanna live at" + address + "?");
 
+    // Google streetmap API
+
     var streetviewUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + "";
     $body.append("<img class='bgimg' src='" + streetviewUrl + "'>");
+
+    // NYTimes API
+
+    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=09f4f95c76cf4bbfb0772dde5d0b8772"
+
+    $.getJSON(nytimesUrl,
+        function (data) {
+            $nytHeaderElem.text("New York Times Articles about" + cityStr);
+            articles = data.response.docs;
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+                $nytElem.append("<li class='articles'>" + "<a href='"+article.web_url+"'>" +article.headline.main+"</a>"+ "<p>"+ article.snippet +"</p>" + "</li>");
+            }
+        }
+    );
 
     return false;
 };
